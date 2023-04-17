ea154a3feb7ff4187c06a89570f885f4c789711d
KsandrGH <112928228+KsandrGH@users.noreply.github.com>
2023-04-17 10:44:40 +0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,31 +4,31 @@ INSERT INTO role (id, name) VALUES (2, 'ADMIN');
 INSERT INTO users (login, password) VALUES ('admin', 'admin');
 
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (1, 'Фантастика', '451° по Фаренгейту', 1199, 'fantasy', 'Рэй Брэдбери', 3, '/img/fantasy1.jpg');
+    VALUES (1, 'Фантастика', 'Солярис', 899, 'fantasy', 'Станислав Лем', 6, '/img/fantasy1.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (2, 'Фантастика', 'О дивный новый мир', 1299, 'fantasy', 'Олдос Хаксли', 5, '/img/fantasy2.jpg');
+    VALUES (2, 'Фантастика', 'Дюна', 699, 'fantasy', 'Фрэнк Герберт', 15, '/img/fantasy2.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (3, 'Фантастика', 'Конец вечности', 1599, 'fantasy', 'Айзек Азимов', 10, '/img/fantasy3.jpg');
+    VALUES (3, 'Фантастика', 'Анафем', 1299, 'fantasy', 'Нил Стивенсон', 8, '/img/fantasy3.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (4, 'Фантастика', 'Властелин колец', 1399, 'fantasy', 'Джон Рональд Руэл Толкин', 8, '/img/fantasy4.jpg');
+    VALUES (4, 'Фантастика', 'Гиперион', 1599, 'fantasy', 'Дэн Симмонс', 3, '/img/fantasy4.jpg');
 
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (5, 'Проза', 'Три товарища', 1499, 'prose', 'Эрих Мария Ремарк', 15, '/img/prose1.jpg');
+    VALUES (5, 'Проза', 'Ненастье', 1199, 'prose', 'Алексей Иванов', 21, '/img/prose1.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (6, 'Проза', 'Евгений Онегин', 899, 'prose', 'Александр Пушкин', 5, '/img/prose2.jpg');
+    VALUES (6, 'Проза', 'День опричника', 1599, 'prose', 'Владимир Сорокин', 2, '/img/prose2.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (7, 'Проза', 'Старик и море', 999, 'prose', 'Эрнест Хемингуэй', 14, '/img/prose3.jpg');
+    VALUES (7, 'Проза', 'Кысь', 599, 'prose', 'Татьяна Толстая', 11, '/img/prose3.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (8, 'Проза', 'Идиот', 1299, 'prose', 'Фёдор Достоевский', 4, '/img/prose4.jpg');
+    VALUES (8, 'Проза', 'Каторга', 799, 'prose', 'Валентин Пикуль', 6, '/img/prose4.jpg');
 
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (9, 'Детектив', 'Остров проклятых', 599, 'detective', 'Деннис Лихэйн', 19, '/img/detective1.jpg');
+    VALUES (9, 'Детектив', 'Убийство на улице Морг', 799, 'detective', 'Эдгар Аллан По', 10, '/img/detective1.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (10, 'Детектив', 'Токийский зодиак', 999, 'detective', 'Симада Содзи', 8, '/img/detective2.jpg');
+    VALUES (10, 'Детектив', 'Лунный камень', 499, 'detective', 'Уилки Коллинз', 4, '/img/detective2.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (11, 'Детектив', 'Убийство в «Восточном экспрессе»', 699, 'detective', 'Агата Кристи', 6, '/img/detective3.jpg');
+    VALUES (11, 'Детектив', 'Загадка Эндхауза', 899, 'detective', 'Агата Кристи', 6, '/img/detective3.jpg');
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
-    VALUES (12, 'Детектив', '1793. История одного убийства', 1099, 'detective', 'Никлас Натт-о-Даг', 3, '/img/detective4.jpg');
+    VALUES (12, 'Детектив', 'Рассказы о Шерлоке Холмсе', 1899, 'detective', 'Артур Конан Дойл', 2, '/img/detective4.jpg');
 
 INSERT INTO book (id, description, name, price, category, author, quantity, image)
     VALUES (13, 'Научно-популярная литература', '21 урок для XXI века', 1199, 'non-fiction', 'Юваль Ной Харари', 8, '/img/non-fiction1.jpg');
